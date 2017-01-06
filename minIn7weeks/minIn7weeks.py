013a9b7a5ad4854d345fad902a86719364e4b549
treadOwnPath <neerajsehgal@gmail.com>
2017-01-06 07:48:16 +0530

    number of minutes in 7 weeks => Completed

--- minIn7weeks/minIn7weeks.py
+++ minIn7weeks/minIn7weeks.py
@@ -1,3 +1,3 @@
-#Intent => print no# minutes in 7 weeks
+# Intent => print no# minutes in 7 weeks
 # minutes in 7 weeks = 7 weeks * 7 days/week * 24 hours/day * 60 min/day
-print (7*7*24*60)
+print(7 * 7 * 24 * 60)
